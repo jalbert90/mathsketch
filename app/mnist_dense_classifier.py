c7f51d33b9cc637e96124b98cd6dbe448b222196
Jacob Albert <jalbert90@gmail.com>
2025-07-27 13:12:45 -0400

    Surpress tensorflow info, build sequential model, fit, and save.

--- app/mnist_dense_classifier.py
+++ app/mnist_dense_classifier.py
@@ -1,13 +1,22 @@
+from os import environ
+
+environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
 from keras.datasets import mnist
 from keras.utils import to_categorical
-from keras import Sequential
-from keras.layers import Flatten, Dense, ReLU, Softmax
+from keras import Sequential, layers
 
 (x_train, y_train), _ = mnist.load_data()
 x_train = x_train / 255.0
 y_train = to_categorical(y_train, num_classes=10)
 
-model = Sequential(
-    Flatten((28, 28)),
-    Dense(128, activation="reLU")
-)
+model = Sequential([
+    layers.Input(shape=(28, 28)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, validation_split=0.2)
+model.save('app/trained_model.keras')
