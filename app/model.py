1da815b3a570107730f50984e2c414f2cba4576c
Jacob Albert <jalbert90@gmail.com>
2025-07-28 00:10:54 -0400

    feat: Add image preprocessing and model loading
    - Load pre-trained model in model.py.
    - Add preprocessing function to convert bytes object to NumPy array in model.py.
    - Update requirements_top.txt to include Pillow.

--- app/model.py
+++ app/model.py
@@ -1,1 +1,13 @@
 from keras.models import load_model
+import numpy as np
+import io
+from PIL import Image
+import PIL
+
+model = load_model('app/trained_model.keras')
+
+def preprocess_image_bytes(image_bytes: bytes) -> np.ndarray:
+    # Wrap the sequence of bytes (image_bytes) with a container that exposes read, write, seek, etc., file operations.
+    image_stream = io.BytesIO(image_bytes)
+
+    image = Image.open(image_stream)
