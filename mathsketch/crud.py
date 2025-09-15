0d526aba64b74c236df322c8f1252eddd67d7f44
Jacob Albert <jalbert90@gmail.com>
2025-09-14 23:30:27 -0400

    Add more endpoints.

--- mathsketch/crud.py
+++ mathsketch/crud.py
@@ -10,13 +10,10 @@ def save_prediction(db: Session, img_data: bytes, pred: int):
     # which is created by PostgreSQL when the db entry is put into the table.
     db.refresh(db_entry)
 
-def delete_prediction(db: Session, id: int):
-    stmt = delete(Prediction).where(Prediction.id==id)
-    db.execute(stmt)
-    db.commit()
-
-def get_predictions(db: Session):
-    # Fetch ORM object Prediction
+def get_all_predictions(db: Session):
+    # Create a `Select` object, which is a Python representation of an SQL SELECT statement.
+    # When executed, will fetch all rows of `predictions` table and return them as
+    # ORM-mapped Prediction objects.
     # Like: SELECT * FROM predictions;
     stmt = select(Prediction)
     # Execute the statement.
@@ -25,4 +22,14 @@ def get_predictions(db: Session):
     # 1 column per row = scalar (as opposed to tuple)
     # SQL executed as we iterate the generator.
     # .all() makes it into a list.
-    results = db.scalars(stmt)
+    results = db.scalars(stmt).all()
+    return results
+
+def delete_prediction(db: Session, id: int):
+    stmt = delete(Prediction).where(Prediction.id==id)
+    db.execute(stmt)
+    db.commit()
+
+def get_predictions(db: Session, limit: int):
+    stmt = select(Prediction).order_by(Prediction.timestamp.desc()).limit(limit)
+    return db.scalars(stmt).all()
--- mathsketch/main.py
+++ mathsketch/main.py
@@ -5,7 +5,9 @@ import base64
 from mathsketch.model import predict_digit
 from mathsketch.db import SessionLocal
 from sqlalchemy.orm import Session
-from mathsketch.crud import save_prediction, delete_prediction
+from mathsketch.crud import save_prediction, get_all_predictions, delete_prediction, get_predictions
+from sqlalchemy import select
+from mathsketch.models import Prediction
 
 app = FastAPI()
 
@@ -23,17 +25,23 @@ class PredictResponse(BaseModel):
     prediction: int
     timestamp: str
 
-# class QueryRequest(BaseModel):
-#     id: int
-
-class QueryResponse(BaseModel):
+class PredictionSchema(BaseModel):
     id: int
     image_data: str
     prediction: int
-    timestamp: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True     # Allow Pydantic to read attributes of ORM object.
+
+class DeleteRequest(BaseModel):
+    ids: list[int]
+
+class HistoryRequest(BaseModel):
+    limit: int
 
 @app.post("/predict", response_model=PredictResponse)
-def predict_endpoint_actions(request: PredictRequest, db: Session = Depends(get_db)):
+def predict_digit(request: PredictRequest, db: Session = Depends(get_db)):
     # FastAPI inspects the function signature ^, prepares the arguments, and then passes them upon calling.
     print(f"Base64 encoded length: {len(request.image_data)} bytes")
 
@@ -46,12 +54,21 @@ def predict_endpoint_actions(request: PredictRequest, db: Session = Depends(get_
 
     # pred = predict_digit(img_bytes)
     pred = 4
-    stamp = datetime.now(timezone.utc).isoformat()      # Prediction made timestamp
+    stamp = datetime.now(timezone.utc).isoformat()      # Prediction made at this time.
     save_prediction(db, img_bytes, pred)
-    # delete_prediction(db, 1)
 
     return PredictResponse(prediction=pred, timestamp=stamp)
 
-@app.get("/query", response_model=QueryResponse)
-def query():
+@app.get("/all", response_model=list[PredictionSchema])
+def get_all(db: Session = Depends(get_db)):
+    return get_all_predictions(db)
+
+@app.delete("/delete")
+def delete_predictions(request: DeleteRequest, db: Session=Depends(get_db)):
+    for id in request.ids:
+        delete_prediction(db, id)
+
+@app.get("/history", response_model=list[PredictionSchema])
+def get_history(request: HistoryRequest, db: Session = Depends(get_db)):
     pass
+    # return get_predictions(db, request.limit)
