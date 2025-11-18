f03aac2bdcb1c73e3515b64ddf0bbfb9849c1d1f
Jacob Albert <jalbert90@gmail.com>
2025-11-17 22:52:36 -0500

    refactor(website): improve comments and fix function definition in main.js

--- static/main.js
+++ static/main.js
@@ -1,8 +1,13 @@
+// Standardize comments.....
+
 // Obtain references to canvas object and drawing info object
 // DPR used to make drawing buffer px width of canvas match real px width of canvas
 const canvas = document.getElementById("draw-canvas");
 const ctx = canvas.getContext("2d");
-const dpr = window.devicePixelRatio;
+
+const dpr = window.devicePixelRatio;    // Capital.....
+
+// Set configs.....
 
 // Set the visible size of the canvas in CSS pixels
 canvas.style.width = 280 + 'px';
@@ -15,12 +20,18 @@ canvas.style.height = 280 + 'px';
 canvas.width = canvas.style.width * dpr;
 canvas.heigth = canvas.style.height * dpr;
 
-// Scale all buffer coordinates so that they match the CSS coordinates
-// Drawing full length of canvas in CSS units should fill the buffer
+// Scale the buffer coordinate system to match the CSS coordinate system.
+// Scaling by the devicePixelRatio ensures that drawing a full canvas size
+// (in CSS units) correctly fills the underlying pixel buffer.
 ctx.scale(dpr, dpr)
 
+// Set the stroke width in CSS pixels.
+// The lineWidth is originally measured in unscaled buffer pixels.
+// Scaling by the DPR makes lineWidth measured in CSS pixels.
+ctx.lineWidth = 5;
+
 // Get position of mouse relative to top-left corner of canvas
-// Inc y is down, inc x is right ??
+// Inc y is down, inc x is right
 function get_mouse_canvas_coordinates(mouse_event){
     const rect = canvas.getBoundingClientRect();
     const x = rect.left;
@@ -28,8 +39,9 @@ function get_mouse_canvas_coordinates(mouse_event){
     return [x, y];
 }
 
-function begin_drawing() {
-
+function begin_drawing(mouse_event) {
+    const [x, y] = get_mouse_canvas_coordinates(mouse_event);
+    ctx.beginPath();
 }
 
 canvas.addEventListener("mousedown", )
