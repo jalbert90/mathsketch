a8e3f3d1ed7c4ebff7889832585c137d6350d9bc
Jacob Albert <jalbert90@gmail.com>
2025-11-18 19:09:14 -0500

    refactor(website): standardize comments and improve variable naming in main.js

--- static/main.js
+++ static/main.js
@@ -1,47 +1,46 @@
-// Standardize comments.....
+const canvas = document.getElementById("draw-canvas");  // Canvas
+const ctx = canvas.getContext("2d");                    // Drawing info
 
-// Obtain references to canvas object and drawing info object
-// DPR used to make drawing buffer px width of canvas match real px width of canvas
-const canvas = document.getElementById("draw-canvas");
-const ctx = canvas.getContext("2d");
+// The devicePixelRatio is used to make the drawing buffer have the same number
+// of pixels as the width of the canvas in real pixels.
+const DPR = window.devicePixelRatio;
 
-const dpr = window.devicePixelRatio;    // Capital.....
+// Config (CSS pixels)
+const CANVAS_SIZE = 280;
+const LINE_WIDTH = 5;
 
-// Set configs.....
+// Set the visible size of the canvas in CSS pixels.
+canvas.style.width = CANVAS_SIZE + 'px';
+canvas.style.height = CANVAS_SIZE + 'px';
 
-// Set the visible size of the canvas in CSS pixels
-canvas.style.width = 280 + 'px';
-canvas.style.height = 280 + 'px';
-
-// Scale the drawing buffer so that there is 1 buffer pixel for every real screen pixel
+// Set the size of the drawing buffer equal to the size of the canvas in real pixels
+// to avoid stretching or blending.
+// # of drawing buffer pxs = # of real pxs
 // Draw in CSS coordinates -> maps to buffer -> buffer mapped to real screen pixels
-// To avoid stretching or blending
-// Assumes that viewport width is set to device-width in the HTML meta tag
-canvas.width = canvas.style.width * dpr;
-canvas.heigth = canvas.style.height * dpr;
+// **This calculation assumes that the viewport width is set to the device-width in an HTML meta tag:
+// <meta name="viewport" content="width=device-width, initial-scale=1">.**
+// device-width = width of screen in CSS pixels
+// width = how big the browswer pretends the viewport is
+canvas.width = canvas.style.width * DPR;
+canvas.heigth = canvas.style.height * DPR;
 
 // Scale the buffer coordinate system to match the CSS coordinate system.
 // Scaling by the devicePixelRatio ensures that drawing a full canvas size
 // (in CSS units) correctly fills the underlying pixel buffer.
-ctx.scale(dpr, dpr)
+ctx.scale(DPR, DPR)
 
 // Set the stroke width in CSS pixels.
 // The lineWidth is originally measured in unscaled buffer pixels.
 // Scaling by the DPR makes lineWidth measured in CSS pixels.
 ctx.lineWidth = 5;
 
-// Get position of mouse relative to top-left corner of canvas
-// Inc y is down, inc x is right
-function get_mouse_canvas_coordinates(mouse_event){
-    const rect = canvas.getBoundingClientRect();
-    const x = rect.left;
-    const y = rect.top;
-    return [x, y];
-}
-
-function begin_drawing(mouse_event) {
-    const [x, y] = get_mouse_canvas_coordinates(mouse_event);
+function begin_drawing(event) {
+    // Get the position of the mouse relative to top-left corner of canvas.
+    // y increases downward, and x increases to the right.
+    const x = event.offsetX;
+    const y = event.offsetY;
     ctx.beginPath();
+    ctx.moveTo(x, y);
 }
 
 canvas.addEventListener("mousedown", )
