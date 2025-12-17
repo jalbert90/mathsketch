dc8a0aaf0e7879354ccbe4a0b12c9d5d5a8f0cac
Jacob Albert <jalbert90@gmail.com>
2025-12-16 21:24:30 -0500

    feat(website): update Dockerfile and db configuration for environment variable support

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,11 @@ FROM python:3.12-slim
 WORKDIR /app
 
 COPY requirements.lock.txt .
+
+RUN pip install -r "requirements.lock.txt"
+
+COPY mathsketch/ .
+
+COPY static/ .
+
+CMD ["uvicorn", "mathsketch.main:app", "--host 0.0.0.0", "--port 8080"]
--- mathsketch/db.py
+++ mathsketch/db.py
@@ -3,10 +3,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_HOST = os.environ['DB_HOST', 'localhost']
+DB_NAME = os.environ['DB_NAME']
 
 # `Engine` used by `Session` to talk to DBAPI (Pyscopg2)
 # Connection resources
-engine = create_engine("postgresql+psycopg2://user:password@localhost:5432/predictions_db")
+engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}')
 
 # Call `Session` (`Session()` = `sessionmaker.__call__(Session)`)
 # to make a new `Session` object bound to an engine and configuration.
