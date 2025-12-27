d9a7860af70d7675b27828ed3942b0571e0534a3
Jacob Albert <jalbert90@gmail.com>
2025-12-27 01:23:26 -0500

    feat(website): make fly-ready

--- mathsketch/db.py
+++ mathsketch/db.py
@@ -5,15 +5,23 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DB_HOST = os.environ['DB_HOST']
-DB_PORT = os.environ.get('DB_PORT', 5432)
-DB_NAME = os.environ['DB_NAME']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
+def get_db_url():
+    DB_URL = os.getenv('DATABASE_URL')
+    
+    if DB_URL:
+        return DB_URL
+    
+    DB_HOST = os.environ['DB_HOST']
+    DB_PORT = os.environ.get('DB_PORT', 5432)
+    DB_NAME = os.environ['DB_NAME']
+    DB_USER = os.environ['DB_USER']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+
+    return f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 # `Engine` used by `Session` to talk to DBAPI (Pyscopg2)
 # Connection resources
-engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+engine = create_engine(get_db_url())
 
 # Call `Session` (`Session()` = `sessionmaker.__call__(Session)`)
 # to make a new `Session` object bound to an engine and configuration.
