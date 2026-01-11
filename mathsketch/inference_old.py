bb3f0676fe52f277d694f7da556f3880a15e6615
Jacob Albert <jalbert90@gmail.com>
2026-01-11 16:07:17 -0500

    refactor: migrate from Keras to ONNX for model inference and update requirements

--- mathsketch/inference_old.py
+++ mathsketch/inference_old.py
@@ -1,10 +1,10 @@
-import onnxruntime as ort
+from keras.models import load_model
 import numpy as np
 import io
 from PIL import Image
 from pathlib import Path
 
-session = ort.InferenceSession('models/trained_model.onnx', providers=['CPUExecutionProvider'])
+model = load_model('models/trained_model.keras')
 
 def preprocess_image_bytes(image_bytes: bytes) -> np.ndarray:
     # Wrap the sequence of bytes (image_bytes) with a container that exposes read, write, seek, etc., file operations.
@@ -13,29 +13,31 @@ def preprocess_image_bytes(image_bytes: bytes) -> np.ndarray:
     # Create image object from file-like object.
     # Pillow is able to parse the stream to make a grid of pixels by looking at the header of the stream.
     img = Image.open(img_stream)
+    
     img = img.resize((28, 28))
 
     # Make image grayscale (L = luminance)
     img = img.convert("L")
+
     img_array = np.array(img)
 
     # Reshape so that model receives the shape it expects.
-    img_array = np.reshape(img_array, (1, 1, 28, 28))
+    img_array = np.reshape(img_array, (1, 28, 28))
 
-    # Save for viewing and debugging
     save_path = Path('digit-preview/drawn_image.png')
     save_path.parent.mkdir(parents=True, exist_ok=True)
-    Image.fromarray(img_array[0][0]).save(save_path)
+    Image.fromarray(img_array[0]).save(save_path)
 
     img_array = img_array / 255.0
+
     return img_array
 
 def predict_digit(image_bytes: bytes) -> int:
     img_array = preprocess_image_bytes(image_bytes)
-
-    outputs = session.run(None, {'input': img_array})
+    probability_array = model.predict(img_array)
     
     # Take index of max and return.
-    index_of_highest_probability = np.argmax(outputs[0])
+    index_of_highest_probability = np.argmax(probability_array)
     # Convert from NumPy int (intp) to regular int.
-    return int(index_of_highest_probability)
+    index_of_highest_probability = int(index_of_highest_probability)
+    return index_of_highest_probability
